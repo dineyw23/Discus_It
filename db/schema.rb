2f26f8077416a58c355db22b7a46e81605a3cdfe
Diney Wankhede <dwankhede@mail.csuchico.edu>
2016-03-29 17:43:34 -0700

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,6 @@ ActiveRecord::Schema.define(version: 20160329225238) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
     t.text     "body"
