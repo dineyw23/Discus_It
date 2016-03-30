d66d52839e396fa6594f1ef5730ae481ed99b56d
Diney Wankhede <dwankhede@mail.csuchico.edu>
2016-03-29 21:11:20 -0700

    Fixed routes issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Rails.application.routes.draw do
   resources :comments
-  resources :comments
-  resources :comments
-  resources :comments
-  resources :comments
-  resources :comments
-  resources :comments
   devise_for :users
   resources :links do
     member do
       put "like",    to: "links#upvote"
       put "dislike", to: "links#downvote"
     end
-    end
+    resources :comments
+  end
 
   root to: "links#index"
 
